42fda43f979100af019ecfdd1a748d6ada192c27
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-10-04 00:26:46 +0600

    rcap

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Nayok></Nayok>
+       <Nayok></Nayok>
+        <Nayok></Nayok>
+         <Nayok></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+function Nayok(){
+  const nayokStyle ={
+    border:'2px solid purple',
+    margin:'20px'
+  }
+   return <div style = {nayokStyle}>
+  <h1> Ami Khol Nayak</h1>
+  <h3>I have done 5 movies</h3>
+  </div>
 }
 
 export default App;
