3b599111cddecea1a176356703c36c7b408d67d2
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-10-05 12:07:59 +0600

    recap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const nayoks = ['jasin','deepjol','bappaRaz']
   return (
     <div className="App">
-      <Nayok></Nayok>
-       <Nayok></Nayok>
-        <Nayok></Nayok>
+      <MovieCounter></MovieCounter>
+      <Nayok name ={nayoks[1]}></Nayok>
+       <Nayok name ="Sakib Khan"></Nayok>
+        <Nayok name ={nayoks.[0]}></Nayok>
          <Nayok></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -14,15 +17,36 @@ function App() {
     </div>
   );
 }
-function Nayok(){
+function MovieCounter(){
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => setCount(count + 1);
+
+  return(
+  <div>
+    <button onClick={handleClick}>Add Movie</button>
+    <h3>Number of Movies: {count}</h3>
+    <MovieDisplay movies={count + 10}></MovieDisplay>
+    <MovieDisplay movies={count + 15}></MovieDisplay>
+    <MovieDisplay movies={count}></MovieDisplay>
+    <MovieDisplay movies={count}></MovieDisplay>
+  </div>
+  )
+}
+function MovieDisplay(props){
+  return <h4>Movies I have acted:{props.movies} </h4>
+}
+function Nayok(props){
   const nayokStyle ={
     border:'2px solid purple',
     margin:'20px'
   }
-   return <div style = {nayokStyle}>
-  <h1> Ami Khol Nayak</h1>
-  <h3>I have done 5 movies</h3>
+   return(
+   <div style = {nayokStyle}>
+  <h1> Ami Nayak-{props.name}</h1>
+  <h3>I have done 5 movies in {props.age}years</h3>
   </div>
+   )
 }
 
 export default App;
